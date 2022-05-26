90aca2c373650e3a0498ae13491f29bffa1749e5
Oliver Holworthy <oliver@holworthy.com>
2022-05-26 21:05:15 +0100

    Add an Operator for Forest Inference Libary (FIL) Triton backend.

---
+++ merlin/systems/dag/ops/fil.py
@@ -0,0 +1,91 @@
+import pathlib
+
+import numpy as np
+from google.protobuf import text_format  # noqa
+
+import merlin.systems.triton.model_config_pb2 as model_config  # noqa
+from merlin.dag import ColumnSelector  # noqa
+from merlin.schema import ColumnSchema, Schema  # noqa
+from merlin.systems.dag.ops.operator import InferenceOperator
+
+
+class FILPredict(InferenceOperator):
+    """Operator for Forest Inference Library (FIL) models.
+
+    Packages up XGBoost models to run on Triton inference server using the fil backend.
+    """
+
+    def __init__(self, model):
+        """Instantiate a FILPredict inference operator.
+
+        Parameters
+        ----------
+        model : xgboost.Booster
+            An XGBoost model.
+        """
+        self.model = model
+        self.input_schema = Schema([ColumnSchema("input__0", dtype=np.int32)])
+        self.output_schema = Schema([ColumnSchema("output__0", dtype=np.int32)])
+        super().__init__()
+
+    def compute_input_schema(
+        self,
+        root_schema: Schema,
+        parents_schema: Schema,
+        deps_schema: Schema,
+        selector: ColumnSelector,
+    ) -> Schema:
+        return self.input_schema
+
+    def compute_output_schema(
+        self, input_schema: Schema, col_selector: ColumnSelector, prev_output_schema: Schema = None
+    ) -> Schema:
+        return self.output_schema
+
+    def export(self, path, input_schema, output_schema, node_id=None, version=1):
+        """Export the model to the supplied path."""
+        node_name = f"{node_id}_{self.export_name}" if node_id is not None else self.export_name
+        node_export_path = pathlib.Path(path) / node_name
+        version_path = node_export_path / str(version)
+        version_path.mkdir(parents=True, exist_ok=True)
+        model_path = version_path / "xgboost.json"
+
+        self.model.save_model(model_path)
+
+        config = model_config.ModelConfig(
+            name=node_name,
+            backend="fil",
+            max_batch_size=8192,
+            input=[
+                model_config.ModelInput(
+                    name="input__0",
+                    data_type=model_config.TYPE_FP32,
+                    dims=[self.model.num_features()],
+                )
+            ],
+            output=[
+                model_config.ModelOutput(
+                    name="output__0", data_type=model_config.TYPE_FP32, dims=[1]
+                )
+            ],
+            instance_group=[
+                model_config.ModelInstanceGroup(kind=model_config.ModelInstanceGroup.Kind.KIND_AUTO)
+            ],
+        )
+
+        default_parameters = {
+            "model_type": "xgboost_json",
+            "predict_proba": "true",
+            "output_class": "true",
+            "threshold": "0.5",
+            "storage_type": "AUTO",
+            "algo": "ALGO_AUTO",
+            "use_experimental_optimizations": "false",
+        }
+        for parameter_key, parameter_value in default_parameters.items():
+            config.parameters[parameter_key].string_value = parameter_value
+
+        with open(node_export_path / "config.pbtxt", "w") as o:
+            text_format.PrintMessage(config, o)
+
+        return config
---
+++ tests/unit/systems/test_fil.py
@@ -0,0 +1,102 @@
+#
+# Copyright (c) 2022, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import pathlib
+from copy import deepcopy
+
+import pytest
+import xgboost as xgb
+
+import merlin.systems.dag.ops.fil as fil_op
+from merlin.dag import ColumnSelector, Graph
+from merlin.schema import Schema
+
+# this needs to be before any modules that import protobuf
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+from google.protobuf import text_format  # noqa
+
+model_config = pytest.importorskip("nvtabular.inference.triton.model_config_pb2")
+
+
+def test_fil_op_exports_own_config(tmpdir):
+    params = {"objective": "binary:logistic"}
+    X = [[1, 2, 3]]
+    y = [0, 1, 0]
+    data = xgb.DMatrix(X, label=y)
+    model = xgb.train(params, data)
+
+    # Triton
+    triton_op = fil_op.FILPredict(model)
+    triton_op.export(tmpdir, None, None)
+
+    # Export creates directory
+    export_path = pathlib.Path(tmpdir) / triton_op.export_name
+    assert export_path.exists()
+
+    # Export creates the config file
+    config_path = export_path / "config.pbtxt"
+    assert config_path.exists()
+
+    # Read the config file back in from proto
+    with open(config_path, "rb") as f:
+        config = model_config.ModelConfig()
+        raw_config = f.read()
+        parsed = text_format.Parse(raw_config, config)
+
+        # The config file contents are correct
+        assert parsed.name == triton_op.export_name
+        assert parsed.backend == "fil"
+
+
+def test_fil_op_compute_schema():
+    params = {"objective": "binary:logistic"}
+    X = [[1, 2, 3]]
+    y = [0, 1, 0]
+    data = xgb.DMatrix(X, label=y)
+    model = xgb.train(params, data)
+
+    # Triton
+    triton_op = fil_op.FILPredict(model)
+
+    out_schema = triton_op.compute_output_schema(
+        Schema(["input__0"]), ColumnSelector(["input__0"]), None
+    )
+    assert out_schema.column_names == ["output__0"]
+
+
+def test_fil_schema_validation():
+    params = {"objective": "binary:logistic"}
+    X = [[1, 2, 3]]
+    y = [0, 1, 0]
+    data = xgb.DMatrix(X, label=y)
+    model = xgb.train(params, data)
+
+    # Triton
+    fil_node = [] >> fil_op.FILPredict(model)
+    fil_graph = Graph(fil_node)
+
+    with pytest.raises(ValueError) as exception_info:
+        deepcopy(fil_graph).construct_schema(Schema([]))
+    assert "Missing column 'input__0'" in str(exception_info.value)
+
+    with pytest.raises(ValueError) as exception_info:
+        deepcopy(fil_graph).construct_schema(Schema(["not_input"]))
+    assert "Missing column 'input__0'" in str(exception_info.value)
+
+    with pytest.raises(ValueError) as exception_info:
+        deepcopy(fil_graph).construct_schema(Schema(["input__0", "not_input"]))
+    assert "Mismatched dtypes for column 'input__0'" in str(exception_info.value)
