ac4ff8869601656a0e1da4324cc91108d642a8d6
Oliver Holworthy <oliver@holworthy.com>
2022-05-30 17:28:54 +0100

    Refactor XGBoost FIL op and add support for LightGBM/Scikit-Learn

--- merlin/systems/dag/ops/fil.py
+++ merlin/systems/dag/ops/fil.py
@@ -1,4 +1,8 @@
+import json
 import pathlib
+import pickle
+import sys
+from abc import ABCMeta, abstractmethod
 
 import numpy as np
 import tritonclient.grpc.model_config_pb2 as model_config  # noqa
@@ -15,15 +19,56 @@ class FIL(InferenceOperator):
     Packages up XGBoost models to run on Triton inference server using the fil backend.
     """
 
-    def __init__(self, model):
+    def __init__(
+        self,
+        model,
+        *,
+        max_batch_size=8192,
+        predict_proba=False,
+        output_class=False,
+        threshold=0.5,
+        algo="ALGO_AUTO",
+        storage_type="AUTO",
+        threads_per_tree=1,
+        blocks_per_sm=0,
+        transfer_threshold=0,
+    ):
         """Instantiate a FILPredict inference operator.
 
         Parameters
         ----------
-        model : xgboost.Booster
-            An XGBoost model.
+        model : Forest model
+            A forest model class. Supports XGBoost, LightGBM, and Scikit-Learn.
+        max_batch_size : int
+           The maximum number of samples to process in a batch. In general, FIL's efficient handling of even large forest models means that this value can be quite high, but this may need to be reduced for your particular hardware configuration if you find that you are exhausting system resources (such as GPU or system RAM).
+        predict_proba : bool
+            If using a classification model. Specifies whether the desired output is a score for each class or merely the predicted class ID. Changes the output size to NUMBER_OF_CLASSES.
+        output_class : bool
+            Is the model a classification model? If set to True will output class ID, unless predict_proba is also to True.
+        threshold : float
+            If using a classification model. The threshold score used for class prediction. Defaults to 0.5.
+        algo : str
+            One of "ALGO_AUTO", "NAIVE", "TREE_REORG" or "BATCH_TREE_REORG" indicating which FIL inference algorithm to use. More details are available in the cuML documentation. If you are uncertain of what algorithm to use, we recommend selecting "ALGO_AUTO", since it is a safe choice for all models.
+        storage_type : str
+            One of "AUTO", "DENSE", "SPARSE", and "SPARSE8", indicating the storage format that should be used to represent the imported model. "AUTO" indicates that the storage format should be automatically chosen. "SPARSE8" is currently experimental.
+        threads_per_tree : int
+            Determines number of threads used to use for inference on a single tree. Increasing this above 1 can improve memory bandwidth near the tree root but use more shared memory. In general, network latency will significantly overshadow any speedup from tweaking this setting, but it is provided for cases where maximizing throughput is essential. for a more thorough explanation of this parameter and how it may be used.
+        blocks_per_sm : int
+             If set to any nonzero value (generally between 2 and 7), this provides a limit to improve the cache hit rate for large forest models. In general, network latency will significantly overshadow any speedup from tweaking this setting, but it is provided for cases where maximizing throughput is essential. Please see the cuML documentation for a more thorough explanation of this parameter and how it may be used.
+        transfer_threshold : int
+             If the number of samples in a batch exceeds this value and the model is deployed on the GPU, then GPU inference will be used. Otherwise, CPU inference will be used for batches received in host memory with a number of samples less than or equal to this threshold. For most models and systems, the default transfer threshold of 0 (meaning that data is always transferred to the GPU for processing) will provide optimal latency and throughput, but for low-latency deployments with the use_experimental_optimizations flag set to true, higher values may be desirable.
         """
-        self.model = model
+        self.max_batch_size = max_batch_size
+        self.parameters = dict(
+            predict_proba=predict_proba,
+            output_class=output_class,
+            algo=algo,
+            transfer_threshold=transfer_threshold,
+            blocks_per_sm=blocks_per_sm,
+            storage_type=storage_type,
+            threshold=threshold,
+        )
+        self.fil_model = get_fil_model(model)
         super().__init__()
 
     def compute_input_schema(
@@ -36,54 +81,265 @@ class FIL(InferenceOperator):
         return Schema([ColumnSchema("input__0", dtype=np.float32)])
 
     def compute_output_schema(
-        self, input_schema: Schema, col_selector: ColumnSelector, prev_output_schema: Schema = None
+        self,
+        input_schema: Schema,
+        col_selector: ColumnSelector,
+        prev_output_schema: Schema = None,
     ) -> Schema:
         return Schema([ColumnSchema("output__0", dtype=np.float32)])
 
     def export(self, path, input_schema, output_schema, node_id=None, version=1):
-        """Export the model to the supplied path."""
+        """Export the model to the supplied path. Returns the config"""
         node_name = f"{node_id}_{self.export_name}" if node_id is not None else self.export_name
         node_export_path = pathlib.Path(path) / node_name
         version_path = node_export_path / str(version)
         version_path.mkdir(parents=True, exist_ok=True)
-        model_path = version_path / "xgboost.json"
 
-        self.model.save_model(model_path)
+        self.fil_model.save(version_path)
 
-        config = model_config.ModelConfig(
-            name=node_name,
-            backend="fil",
-            max_batch_size=8192,
-            input=[
-                model_config.ModelInput(
-                    name="input__0",
-                    data_type=model_config.TYPE_FP32,
-                    dims=[self.model.num_features()],
-                )
-            ],
-            output=[
-                model_config.ModelOutput(
-                    name="output__0", data_type=model_config.TYPE_FP32, dims=[1]
-                )
-            ],
-            instance_group=[
-                model_config.ModelInstanceGroup(kind=model_config.ModelInstanceGroup.Kind.KIND_AUTO)
-            ],
+        config = fil_config(
+            node_name,
+            self.fil_model.model_type,
+            self.fil_model.num_features,
+            self.fil_model.num_classes,
+            max_batch_size=self.max_batch_size,
+            **self.parameters,
         )
-
-        default_parameters = {
-            "model_type": "xgboost_json",
-            "predict_proba": "true",
-            "output_class": "true",
-            "threshold": "0.5",
-            "storage_type": "AUTO",
-            "algo": "ALGO_AUTO",
-            "use_experimental_optimizations": "false",
-        }
-        for parameter_key, parameter_value in default_parameters.items():
-            config.parameters[parameter_key].string_value = parameter_value
 
         with open(node_export_path / "config.pbtxt", "w") as o:
             text_format.PrintMessage(config, o)
 
         return config
+
+
+class FILModel(metaclass=ABCMeta):
+    """Interface for a FIL Model. Methods implment required information to construct a FIL
+    model configuration file"""
+
+    def __init__(self, model):
+        self.model = model
+
+    @abstractmethod
+    def save(self, version_path):
+        """
+        Save model to version_path
+        """
+
+    @property
+    @abstractmethod
+    def num_classes(self):
+        """
+        The number of classses
+        """
+
+    @property
+    @abstractmethod
+    def num_features(self):
+        """
+        The number of features
+        """
+
+    @property
+    @abstractmethod
+    def num_targets(self):
+        """
+        The number of targets
+        """
+
+    @property
+    @abstractmethod
+    def model_type(self):
+        """The type of model"""
+
+
+def get_fil_model(model) -> FILModel:
+    """Return FILModel class corresponding to the model passed in.
+
+    Parameters
+    ----------
+    model
+        A model class.
+
+        Supports:
+            - XGBoost {Booster, XGBClassifier, XGBRegressor}
+            - LightGBM {Booster}
+            - Scikit-Learn {RandomForestClassifier, RandomForestRegressor}
+    """
+    if "xgboost" in sys.modules and isinstance(model, sys.modules["xgboost"].Booster):
+        fil_model = XGBoost(model)
+    elif "xgboost" in sys.modules and isinstance(
+        model, (sys.modules["xgboost"].XGBClassifier, sys.modules["xgboost"].XGBRegressor)
+    ):
+        fil_model = XGBoost(model.get_booster())
+    elif "lightgbm" in sys.modules and isinstance(model, sys.modules["lightgbm"].Booster):
+        fil_model = LightGBM(model)
+    elif "sklearn" in sys.modules and isinstance(
+        model,
+        (
+            sys.modules["sklearn"].ensemble.RandomForestClassifier,
+            sys.modules["sklearn"].ensemble.RandomForestRegressor,
+        ),
+    ):
+        fil_model = SKLearnRandomForest(model)
+    else:
+        raise ValueError(
+            "Model type not supported. Must be one of {xgboost.Booster, lightgbm.Booster, sklearn.ensemble.RandomForestClassifier, sklearn.ensemble.RandomForestRegressor}"
+        )
+    return fil_model
+
+
+class XGBoost(FILModel):
+    model_type = "xgboost_json"
+    model_filename = "xgboost.json"
+
+    def __init__(self, model):
+        self.model = model
+        learner = json.loads(model.save_config())["learner"]
+        self._learner = learner
+
+        objective = learner["objective"]["name"]
+        if objective == "binary:hinge":
+            raise ValueError(
+                "Objective binary:hinge is not supported. Only sigmoid and identity values of pred_transform are supported for binary classification"
+            )
+
+        learner_model_param = learner["learner_model_param"]
+        num_targets = int(learner_model_param["num_target"])
+
+        if num_targets > 1:
+            raise ValueError("Only single target objectives are supported.")
+
+    def save(self, version_path) -> None:
+        model_path = pathlib.Path(version_path) / self.model_filename
+        self.model.save_model(model_path)
+
+    def _get_learner(self):
+        return self._learner
+
+    @property
+    def num_classes(self):
+        learner = self._get_learner()
+        learner_model_param = learner["learner_model_param"]
+        num_classes = int(learner_model_param["num_class"])
+        return num_classes
+
+    @property
+    def num_features(self):
+        return self.model.num_features()
+
+    @property
+    def num_targets(self):
+        learner = self._get_learner()
+        learner_model_param = learner["learner_model_param"]
+        num_targets = int(learner_model_param["num_target"])
+        return num_targets
+
+
+class LightGBM(FILModel):
+    model_type = "lightgbm"
+    model_filename = "model.txt"
+
+    def save(self, version_path):
+        model_path = pathlib.Path(version_path) / self.model_filename
+        self.model.save_model(model_path)
+
+    @property
+    def num_features(self):
+        return self.model.num_feature()
+
+    @property
+    def num_classes(self):
+        return self.model.dump_model()["num_class"]
+
+    @property
+    def num_targets(self):
+        return 1
+
+
+class SKLearnRandomForest:
+    model_type = "treelite_checkpoint"
+    model_filename = "model.pkl"
+
+    def save(self, version_path):
+        model_path = pathlib.Path(version_path) / self.model_filename
+        with open(model_path, "wb") as model_file:
+            pickle.dump(self.model, model_file)
+
+    @property
+    def num_features(self):
+        return self.model.n_features_in_
+
+    @property
+    def num_classes(self):
+        return self.model.n_classes_
+
+    @property
+    def num_targets(self):
+        return self.model.n_outputs_
+
+
+def fil_config(
+    name,
+    model_type,
+    num_features,
+    num_classes,
+    *,
+    max_batch_size=8192,
+    predict_proba=False,
+    output_class=False,
+    threshold=0.5,
+    algo="ALGO_AUTO",
+    storage_type="AUTO",
+    blocks_per_sm=0,
+    threads_per_tree=1,
+    transfer_threshold=0,
+) -> model_config.ModelConfig:
+
+    input_dim = num_features
+    output_dim = 1
+
+    # if we have multiclass then switch to output_class
+    if num_classes > 2:
+        output_class = True
+
+    if output_class and predict_proba:
+        output_dim = num_classes
+
+    parameters = {
+        "model_type": model_type,
+        "predict_proba": "true" if predict_proba else "false",
+        "output_class": "true" if output_class else "false",
+        "threshold": f"{threshold:.4f}",
+        "storage_type": storage_type,
+        "algo": algo,
+        "use_experimental_optimizations": "false",
+        "blocks_per_sm": f"{blocks_per_sm:d}",
+        "threads_per_tree": f"{threads_per_tree:d}",
+        "transfer_threshold": f"{transfer_threshold:d}",
+    }
+
+    config = model_config.ModelConfig(
+        name=name,
+        backend="fil",
+        max_batch_size=max_batch_size,
+        input=[
+            model_config.ModelInput(
+                name="input__0",
+                data_type=model_config.TYPE_FP32,
+                dims=[input_dim],
+            )
+        ],
+        output=[
+            model_config.ModelOutput(
+                name="output__0", data_type=model_config.TYPE_FP32, dims=[output_dim]
+            )
+        ],
+        instance_group=[
+            model_config.ModelInstanceGroup(kind=model_config.ModelInstanceGroup.Kind.KIND_AUTO)
+        ],
+    )
+
+    for parameter_key, parameter_value in parameters.items():
+        config.parameters[parameter_key].string_value = parameter_value
+
+    return config
