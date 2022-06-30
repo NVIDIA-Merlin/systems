d7e1382e69cfc1056cb484ce233eace3c82498e1
Julio Perez <jperez@nvidia.com>
2022-06-30 12:04:31 -0400

    hugectr op is green for single hot columns

--- tests/unit/systems/hugectr/test_hugectr.py
+++ tests/unit/systems/hugectr/test_hugectr.py
@@ -14,14 +14,18 @@
 # limitations under the License.
 #
 
-import gc
 import os
-import shutil
-from os import path
 
 import cudf
+import numpy as np
+import pytest
 
+import nvtabular as nvt
+from merlin.dag import ColumnSelector
+from merlin.schema import ColumnSchema, Schema
+from merlin.systems.dag.ensemble import Ensemble
 from merlin.systems.dag.ops.hugectr import HugeCTR
+from tests.unit.systems.utils.triton import _run_ensemble_on_tritonserver
 
 try:
     import hugectr
@@ -30,52 +34,28 @@ try:
 except ImportError:
     hugectr = None
 
-from distutils.spawn import find_executable
 
-import numpy as np
-import pandas as pd
-
+triton = pytest.importorskip("merlin.systems.triton")
+grpcclient = pytest.importorskip("tritonclient.grpc")
 # from common.parsers.benchmark_parsers import create_bench_result
 # from common.utils import _run_query
-from sklearn.model_selection import train_test_split
-
-import nvtabular as nvt
-from merlin.core.utils import download_file
-from nvtabular.ops import get_embedding_sizes
-
-DIR = "/raid/data/movielens/data/"
-DATA_DIR = DIR + "data/"
-TEMP_DIR = DIR + "temp_hugectr/"
-MODEL_DIR = DIR + "models/"
-TRAIN_DIR = MODEL_DIR + "test_model/1/"
-NETWORK_FILE = TRAIN_DIR + "model.json"
-DENSE_FILE = TRAIN_DIR + "_dense_1900.model"
-SPARSE_FILES = TRAIN_DIR + "0_sparse_1900.model"
-MODEL_NAME = "test_model"
-
-CATEGORICAL_COLUMNS = ["userId", "movieId", "new_cat1"]
-LABEL_COLUMNS = ["rating"]
-TEST_N_ROWS = 64
-
-TRITON_SERVER_PATH = find_executable("tritonserver")
-TRITON_DEVICE_ID = "1"
 
 
-def _run_model(slot_sizes, total_cardinality):
+def _run_model(slot_sizes, source, dense_dim):
     solver = hugectr.CreateSolver(
         vvgpu=[[0]],
-        batchsize=2048,
-        batchsize_eval=2048,
-        max_eval_batches=160,
+        batchsize=10,
+        batchsize_eval=10,
+        max_eval_batches=50,
         i64_input_key=True,
         use_mixed_precision=False,
         repeat_dataset=True,
     )
-
+    # https://github.com/NVIDIA-Merlin/HugeCTR/blob/9e648f879166fc93931c676a5594718f70178a92/docs/source/api/python_interface.md#datareaderparams
     reader = hugectr.DataReaderParams(
         data_reader_type=hugectr.DataReaderType_t.Parquet,
-        source=[DATA_DIR + "train/_file_list.txt"],
-        eval_source=DATA_DIR + "valid/_file_list.txt",
+        source=[os.path.join(source, "_file_list.txt")],
+        eval_source=os.path.join(source, "_file_list.txt"),
         check_type=hugectr.Check_t.Non,
     )
 
@@ -86,14 +66,13 @@ def _run_model(slot_sizes, total_cardinality):
         hugectr.Input(
             label_dim=1,
             label_name="label",
-            dense_dim=0,
+            dense_dim=dense_dim,
             dense_name="dense",
             data_reader_sparse_param_array=[
                 hugectr.DataReaderSparseParam("data1", len(slot_sizes) + 1, True, len(slot_sizes))
             ],
         )
     )
-
     model.add(
         hugectr.SparseEmbedding(
             embedding_type=hugectr.Embedding_t.DistributedSlotSparseEmbeddingHash,
@@ -108,6 +87,14 @@ def _run_model(slot_sizes, total_cardinality):
     )
     model.add(
         hugectr.DenseLayer(
+            layer_type=hugectr.Layer_t.InnerProduct,
+            bottom_names=["dense"],
+            top_names=["fc1"],
+            num_output=512,
+        )
+    )
+    model.add(
+        hugectr.DenseLayer(
             layer_type=hugectr.Layer_t.Reshape,
             bottom_names=["sparse_embedding1"],
             top_names=["reshape1"],
@@ -117,196 +104,169 @@ def _run_model(slot_sizes, total_cardinality):
     model.add(
         hugectr.DenseLayer(
             layer_type=hugectr.Layer_t.InnerProduct,
-            bottom_names=["reshape1"],
-            top_names=["fc1"],
-            num_output=128,
-        )
-    )
-    model.add(
-        hugectr.DenseLayer(
-            layer_type=hugectr.Layer_t.ReLU,
-            bottom_names=["fc1"],
-            top_names=["relu1"],
-        )
-    )
-    model.add(
-        hugectr.DenseLayer(
-            layer_type=hugectr.Layer_t.InnerProduct,
-            bottom_names=["relu1"],
+            bottom_names=["reshape1", "fc1"],
             top_names=["fc2"],
-            num_output=128,
-        )
-    )
-    model.add(
-        hugectr.DenseLayer(
-            layer_type=hugectr.Layer_t.ReLU,
-            bottom_names=["fc2"],
-            top_names=["relu2"],
-        )
-    )
-    model.add(
-        hugectr.DenseLayer(
-            layer_type=hugectr.Layer_t.InnerProduct,
-            bottom_names=["relu2"],
-            top_names=["fc3"],
             num_output=1,
         )
     )
     model.add(
         hugectr.DenseLayer(
             layer_type=hugectr.Layer_t.BinaryCrossEntropyLoss,
-            bottom_names=["fc3", "label"],
+            bottom_names=["fc2", "label"],
             top_names=["loss"],
         )
     )
     model.compile()
     model.summary()
     model.fit(max_iter=20, display=100, eval_interval=200, snapshot=10)
-    model.graph_to_json(graph_config_file=NETWORK_FILE)
 
     return model
 
 
-def _predict(dense_features, embedding_columns, row_ptrs, config_file, model_name):
+def _convert(data, slot_size_array, categorical_columns, labels=None):
+    labels = labels or []
+    dense_columns = list(set(data.columns) - set(categorical_columns + labels))
+    categorical_dim = len(categorical_columns)
+    batch_size = data.shape[0]
+
+    shift = np.insert(np.cumsum(slot_size_array), 0, 0)[:-1].tolist()
+
+    # These dtypes are static for HugeCTR
+    dense = np.array([data[dense_columns].values.flatten().tolist()], dtype="float32")
+    cat = np.array([(data[categorical_columns] + shift).values.flatten().tolist()], dtype="int64")
+    rowptr = np.array([list(range(batch_size * categorical_dim + 1))], dtype="int32")
+
+    return dense, cat, rowptr
+
+
+def test_training(tmpdir):
+    cat_dtypes = {"a": int, "b": int, "c": int}
+    dataset = cudf.datasets.randomdata(1, dtypes={**cat_dtypes, "label": bool})
+    dataset["label"] = dataset["label"].astype("int32")
+
+    categorical_columns = list(cat_dtypes.keys())
+
+    gdf = cudf.DataFrame(
+        {
+            "a": np.arange(64),
+            "b": np.arange(64),
+            "c": np.arange(64),
+            "d": np.random.rand(64).tolist(),
+            "label": [0] * 64,
+        },
+        dtype="int64",
+    )
+    gdf["label"] = gdf["label"].astype("float32")
+    train_dataset = nvt.Dataset(gdf)
+
+    dense_columns = ["d"]
+
+    dict_dtypes = {}
+    for col in dense_columns:
+        dict_dtypes[col] = np.float32
+
+    for col in categorical_columns:
+        dict_dtypes[col] = np.int64
+
+    for col in ["label"]:
+        dict_dtypes[col] = np.float32
+
+    train_path = os.path.join(tmpdir, "train/")
+    os.mkdir(train_path)
+
+    train_dataset.to_parquet(
+        output_path=train_path,
+        shuffle=nvt.io.Shuffle.PER_PARTITION,
+        cats=categorical_columns,
+        conts=dense_columns,
+        labels=["label"],
+        dtypes=dict_dtypes,
+    )
+
+    embeddings = {"a": (64, 16), "b": (64, 16), "c": (64, 16)}
+
+    total_cardinality = 0
+    slot_sizes = []
+
+    for column in cat_dtypes:
+        slot_sizes.append(embeddings[column][0])
+        total_cardinality += embeddings[column][0]
+
+    # slot sizes = list of caridinalities per column, total is sum of individual
+    model = _run_model(slot_sizes, train_path, len(dense_columns))
+
+    model_op = HugeCTR(model, max_nnz=2, device_list=[0])
+
+    model_repository_path = os.path.join(tmpdir, "model_repository")
+
+    input_schema = Schema(
+        [
+            ColumnSchema("DES", dtype=np.float32),
+            ColumnSchema("CATCOLUMN", dtype=np.int64),
+            ColumnSchema("ROWINDEX", dtype=np.int32),
+        ]
+    )
+    triton_chain = ColumnSelector(["DES", "CATCOLUMN", "ROWINDEX"]) >> model_op
+    ens = Ensemble(triton_chain, input_schema)
+
+    os.makedirs(model_repository_path)
+
+    enc_config, node_configs = ens.export(model_repository_path)
+
+    assert enc_config
+    assert len(node_configs) == 1
+    assert node_configs[0].name == "0_hugectr"
+
+    df = train_dataset.to_ddf().compute()[:5]
+    dense, cats, rowptr = _convert(df, slot_sizes, categorical_columns, labels=["label"])
+
+    inputs = [
+        grpcclient.InferInput("DES", dense.shape, triton.np_to_triton_dtype(dense.dtype)),
+        grpcclient.InferInput("CATCOLUMN", cats.shape, triton.np_to_triton_dtype(cats.dtype)),
+        grpcclient.InferInput("ROWINDEX", rowptr.shape, triton.np_to_triton_dtype(rowptr.dtype)),
+    ]
+    inputs[0].set_data_from_numpy(dense)
+    inputs[1].set_data_from_numpy(cats)
+    inputs[2].set_data_from_numpy(rowptr)
+
+    response = _run_ensemble_on_tritonserver(
+        model_repository_path,
+        ["OUTPUT0"],
+        inputs,
+        "0_hugectr",
+        backend_config=f"hugectr,ps={tmpdir}/model_repository/ps.json",
+    )
+    assert len(response.as_numpy("OUTPUT0")) == df.shape[0]
+
+    model_config = node_configs[0].parameters["config"].string_value
+
+    hugectr_name = node_configs[0].name
+    dense_path = f"{tmpdir}/model_repository/{hugectr_name}/1/_dense_0.model"
+    sparse_files = [f"{tmpdir}/model_repository/{hugectr_name}/1/0_sparse_0.model"]
+    out_predict = _predict(
+        dense, cats, rowptr, model_config, hugectr_name, dense_path, sparse_files
+    )
+
+    np.testing.assert_array_almost_equal(response.as_numpy("OUTPUT0"), np.array(out_predict))
+
+
+def _predict(
+    dense_features, embedding_columns, row_ptrs, config_file, model_name, dense_path, sparse_paths
+):
     inference_params = InferenceParams(
         model_name=model_name,
         max_batchsize=64,
         hit_rate_threshold=0.5,
-        dense_model_file=DENSE_FILE,
-        sparse_model_files=[SPARSE_FILES],
+        dense_model_file=dense_path,
+        sparse_model_files=sparse_paths,
         device_id=0,
         use_gpu_embedding_cache=True,
-        cache_size_percentage=0.1,
+        cache_size_percentage=0.2,
         i64_input_key=True,
         use_mixed_precision=False,
     )
     inference_session = CreateInferenceSession(config_file, inference_params)
-    output = inference_session.predict(dense_features, embedding_columns, row_ptrs)  # , True)
-
-    test_data_path = DATA_DIR + "test/"
-    embedding_columns_df = pd.DataFrame()
-    embedding_columns_df["embedding_columns"] = embedding_columns
-    embedding_columns_df.to_csv(test_data_path + "embedding_columns.csv")
-
-    row_ptrs_df = pd.DataFrame()
-    row_ptrs_df["row_ptrs"] = row_ptrs
-    row_ptrs_df.to_csv(test_data_path + "row_ptrs.csv")
-
-    output_df = pd.DataFrame()
-    output_df["output"] = output
-    output_df.to_csv(test_data_path + "output.csv")
-
-
-def _convert(data, slot_size_array):
-    categorical_dim = len(CATEGORICAL_COLUMNS)
-    batch_size = data.shape[0]
-
-    offset = np.insert(np.cumsum(slot_size_array), 0, 0)[:-1].tolist()
-    data[CATEGORICAL_COLUMNS] += offset
-    cat = data[CATEGORICAL_COLUMNS].values.reshape(1, batch_size * categorical_dim).tolist()[0]
-
-    row_ptrs = list(range(batch_size * categorical_dim + 1))
-    dense = []
-
-    return dense, cat, row_ptrs
-
-
-def test_training(tmpdir):
-    # Download & Convert data
-    download_file(
-        "http://files.grouplens.org/datasets/movielens/ml-25m.zip",
-        os.path.join(DATA_DIR, "ml-25m.zip"),
+    output = inference_session.predict(
+        dense_features[0].tolist(), embedding_columns[0].tolist(), row_ptrs[0].tolist()
     )
-
-    ratings = cudf.read_csv(os.path.join(DATA_DIR, "ml-25m", "ratings.csv"))
-    ratings["new_cat1"] = ratings["userId"] / ratings["movieId"]
-    ratings["new_cat1"] = ratings["new_cat1"].astype("int64")
-    ratings.head()
-
-    ratings = ratings.drop("timestamp", axis=1)
-    train, valid = train_test_split(ratings, test_size=0.2, random_state=42)
-
-    train.to_parquet(DATA_DIR + "train.parquet")
-    valid.to_parquet(DATA_DIR + "valid.parquet")
-
-    del train
-    del valid
-    gc.collect()
-
-    # Perform ETL with NVTabular
-    cat_features = CATEGORICAL_COLUMNS >> nvt.ops.Categorify(cat_cache="device")
-    ratings = nvt.ColumnSelector(["rating"]) >> nvt.ops.LambdaOp(
-        lambda col: (col > 3).astype("int8")
-    )
-    output = cat_features + ratings
-
-    workflow = nvt.Workflow(output)
-
-    train_dataset = nvt.Dataset(DATA_DIR + "train.parquet", part_size="100MB")
-    valid_dataset = nvt.Dataset(DATA_DIR + "valid.parquet", part_size="100MB")
-
-    workflow.fit(train_dataset)
-
-    dict_dtypes = {}
-
-    for col in CATEGORICAL_COLUMNS:
-        dict_dtypes[col] = np.int64
-
-    for col in LABEL_COLUMNS:
-        dict_dtypes[col] = np.float32
-
-    if path.exists(DATA_DIR + "train"):
-        shutil.rmtree(os.path.join(DATA_DIR, "train"))
-    if path.exists(DATA_DIR + "valid"):
-        shutil.rmtree(os.path.join(DATA_DIR, "valid"))
-
-    workflow.transform(train_dataset).to_parquet(
-        output_path=DATA_DIR + "train/",
-        shuffle=nvt.io.Shuffle.PER_PARTITION,
-        cats=CATEGORICAL_COLUMNS,
-        labels=LABEL_COLUMNS,
-        dtypes=dict_dtypes,
-    )
-    workflow.transform(valid_dataset).to_parquet(
-        output_path=DATA_DIR + "valid/",
-        shuffle=False,
-        cats=CATEGORICAL_COLUMNS,
-        labels=LABEL_COLUMNS,
-        dtypes=dict_dtypes,
-    )
-
-    # Train with HugeCTR
-    embeddings = get_embedding_sizes(workflow)
-    total_cardinality = 0
-    slot_sizes = []
-    for column in CATEGORICAL_COLUMNS:
-        slot_sizes.append(embeddings[column][0])
-        total_cardinality += embeddings[column][0]
-
-    test_data_path = DATA_DIR + "test/"
-    if path.exists(test_data_path):
-        shutil.rmtree(test_data_path)
-
-    os.mkdir(test_data_path)
-
-    if path.exists(MODEL_DIR):
-        shutil.rmtree(MODEL_DIR)
-
-    os.makedirs(TRAIN_DIR)
-
-    sample_data = cudf.read_parquet(DATA_DIR + "valid.parquet", num_rows=TEST_N_ROWS)
-    sample_data.to_csv(test_data_path + "data.csv")
-
-    sample_data_trans = nvt.workflow.workflow._transform_partition(
-        sample_data, [workflow.output_node]
-    )
-
-    dense_features, embedding_columns, row_ptrs = _convert(sample_data_trans, slot_sizes)
-
-    model = _run_model(slot_sizes, total_cardinality)
-
-    model_op = HugeCTR(model)
-    config = model_op.export(tmpdir, None, None)
-    assert config is not None
-
-    # _predict(dense_features, embedding_columns, row_ptrs, tmpdir, MODEL_NAME)
+    return output
