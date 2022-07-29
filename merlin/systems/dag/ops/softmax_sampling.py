48ddd15ba3d24196c2987b2ae8cadb2e0856f11c
Adam Laiacano <alaiacano@nvidia.com>
2022-07-29 15:58:35 -0400

    rename old variable

--- merlin/systems/dag/ops/softmax_sampling.py
+++ merlin/systems/dag/ops/softmax_sampling.py
@@ -130,7 +130,7 @@ class SoftmaxSampling(PipelineableInferenceOperator):
 
         # This is just bookkeeping to produce the final ordered list of recs
         sorted_indices = np.argsort(exponentials)
-        topk_movie_ids = candidate_ids[sorted_indices][: self.topk]
-        ordered_movie_ids = topk_movie_ids.reshape(1, -1).T
+        topk_item_ids = candidate_ids[sorted_indices][: self.topk]
+        ordered_item_ids = topk_item_ids.reshape(1, -1).T
 
-        return InferenceDataFrame({"ordered_ids": ordered_movie_ids})
+        return InferenceDataFrame({"ordered_ids": ordered_item_ids})
