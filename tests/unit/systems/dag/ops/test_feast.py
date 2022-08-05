becb05e34ab8c0f20e82b4e652ee763879e3ac11
Adam Laiacano <alaiacano@nvidia.com>
2022-08-05 09:54:29 -0400

    fix mocks to not modify global variables

--- tests/unit/systems/dag/ops/test_feast.py
+++ tests/unit/systems/dag/ops/test_feast.py
@@ -1,9 +1,8 @@
 import json
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import numpy as np
 import pytest
-from feast import FeatureStore
 from feast.online_response import OnlineResponse
 from feast.protos.feast.serving import ServingService_pb2
 from feast.protos.feast.types import Value_pb2
@@ -11,6 +10,46 @@ from feast.protos.feast.types import Value_pb2
 from merlin.schema import ColumnSchema, Schema
 from merlin.systems.dag.ops.feast import QueryFeast
 from merlin.systems.dag.ops.operator import InferenceDataFrame
+
+
+def test_feast_config_round_trip():
+    """
+    This builds a QueryFeast op via the constructor, exports the config, and then builds another
+    QueryFeast op via from_config to ensure that the constructor arguments match the original.
+    """
+    input_schema = Schema()
+    output_schema = Schema()
+
+    # We need to mock the FeatureStore constructor so it doesn't look for a real store, and the
+    # QueryFeast constructor so that we can ensure that it gets called with the correct args.
+    # These must be done in a context manager so we don't modify the classes globally and
+    # affect other tests.
+    with patch("feast.FeatureStore.__init__", MagicMock(return_value=None)):
+        with patch(
+            "merlin.systems.dag.ops.feast.__init__", MagicMock(return_value=None)
+        ) as qf_init:
+
+            # Define the args & kwargs. We want to ensure the round-tripped version uses these same
+            # arguments.
+            args = [
+                "repo_path",
+                "entity_id",
+                "entity_view",
+                "entity_column",
+                ["features"],
+                ["mh_features"],
+                input_schema,
+                output_schema,
+            ]
+            kwargs = {"include_id": True, "output_prefix": "prefix"}
+            feast_op = QueryFeast(*args, **kwargs)
+
+            created_config = feast_op.export("export_path", input_schema, output_schema)
+            created_config_dict = json.loads(created_config.parameters["queryfeast"].string_value)
+
+            # now mock the QueryFeast constructor so we can inspect its arguments.
+            QueryFeast.from_config(created_config_dict)
+            assert qf_init.called_with(*args, **kwargs)
 
 
 @pytest.mark.parametrize("is_ragged", [True, False])
@@ -35,79 +74,39 @@ def test_feast_transform(prefix, is_ragged):
         )
     )
 
-    FeatureStore.__init__ = MagicMock(return_value=None)
-    FeatureStore.get_online_features = MagicMock(return_value=mocked_resp)
+    with patch("feast.FeatureStore.__init__", MagicMock(return_value=None)):
+        with patch("feast.FeatureStore.get_online_features", MagicMock(return_value=mocked_resp)):
 
-    # names of the features with prefix/suffix
-    feature_name = f"{prefix}_feature" if prefix else "feature"
-    feature_mh_1 = f"{prefix}_mh_feature_1" if prefix else "mh_feature_1"
-    feature_mh_2 = f"{prefix}_mh_feature_2" if prefix else "mh_feature_2"
+            # names of the features with prefix/suffix
+            feature_name = f"{prefix}_feature" if prefix else "feature"
+            feature_mh_1 = f"{prefix}_mh_feature_1" if prefix else "mh_feature_1"
+            feature_mh_2 = f"{prefix}_mh_feature_2" if prefix else "mh_feature_2"
 
-    input_schema = Schema([ColumnSchema("feature"), ColumnSchema("mh_feature")])
-    output_schema = Schema(
-        [
-            ColumnSchema(feature_name),
-            ColumnSchema(feature_mh_1, is_list=True, is_ragged=is_ragged),
-            ColumnSchema(feature_mh_2, is_list=True, is_ragged=is_ragged),
-        ]
-    )
+            input_schema = Schema([ColumnSchema("feature"), ColumnSchema("mh_feature")])
+            output_schema = Schema(
+                [
+                    ColumnSchema(feature_name),
+                    ColumnSchema(feature_mh_1, is_list=True, is_ragged=is_ragged),
+                    ColumnSchema(feature_mh_2, is_list=True, is_ragged=is_ragged),
+                ]
+            )
 
-    feast_op = QueryFeast(
-        "repo_path",
-        "entity_id",
-        "entity_view",
-        "entity_id",
-        ["feature"],
-        ["mh_feature"],
-        input_schema,
-        output_schema,
-        include_id=True,
-        output_prefix=prefix,
-    )
+            feast_op = QueryFeast(
+                "repo_path",
+                "entity_id",
+                "entity_view",
+                "entity_id",
+                ["feature"],
+                ["mh_feature"],
+                input_schema,
+                output_schema,
+                include_id=True,
+                output_prefix=prefix,
+            )
 
-    df = InferenceDataFrame({"entity_id": [1]})
-    resp = feast_op.transform(df)
-    assert resp["entity_id"] == [1]
-    assert resp[feature_name] == np.array([[1.0]])
-    assert np.all(resp[feature_mh_1] == np.array([[1.0], [2.0], [3.0]]))
-    assert resp[feature_mh_2] == np.array([[3.0]])
-
-
-def test_feast_config_round_trip():
-    """
-    This builds a QueryFeast op via the constructor, exports the config, and then builds another
-    QueryFeast op via from_config to ensure that the constructor arguments match the original.
-
-
-    This is a bad look, but this test needs to be last in the file because we mock
-    QueryFeast.__init__, which somehow globally modifies that class. So other tests that call
-    the QueryFeast constructor must do so before this test.
-    """
-    input_schema = Schema()
-    output_schema = Schema()
-
-    # Need to mock the FeatureStore constructor so it doesn't look for a real store
-    FeatureStore.__init__ = MagicMock(return_value=None)
-
-    # Define the args & kwargs. We want to ensure the round-tripped version uses these same
-    # arguments.
-    args = [
-        "repo_path",
-        "entity_id",
-        "entity_view",
-        "entity_column",
-        ["features"],
-        ["mh_features"],
-        input_schema,
-        output_schema,
-    ]
-    kwargs = {"include_id": True, "output_prefix": "prefix"}
-    feast_op = QueryFeast(*args, **kwargs)
-
-    created_config = feast_op.export("export_path", input_schema, output_schema)
-    created_config_dict = json.loads(created_config.parameters["queryfeast"].string_value)
-
-    # now mock the QueryFeast constructor so we can inspect its arguments.
-    QueryFeast.__init__ = MagicMock(return_value=None)
-    QueryFeast.from_config(created_config_dict)
-    assert QueryFeast.__init__.called_with(*args, **kwargs)
+            df = InferenceDataFrame({"entity_id": [1]})
+            resp = feast_op.transform(df)
+            assert resp["entity_id"] == [1]
+            assert resp[feature_name] == np.array([[1.0]])
+            assert np.all(resp[feature_mh_1] == np.array([[1.0], [2.0], [3.0]]))
+            assert resp[feature_mh_2] == np.array([[3.0]])
