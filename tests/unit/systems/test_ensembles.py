1a7215e4acc247903ff6ae2bbce3e6209c047257
Karl Higley <kmhigley@gmail.com>
2022-08-16 12:52:05 -0400

    Move XGBoost ensemble test into `fil` directory

--- tests/unit/systems/test_ensembles.py
+++ tests/unit/systems/test_ensembles.py
@@ -16,11 +16,7 @@
 import os
 from distutils.spawn import find_executable
 
-import numpy as np
-import pandas as pd
 import pytest
-import sklearn.datasets
-import xgboost
 
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
@@ -28,9 +24,7 @@ from google.protobuf import text_format  # noqa
 
 from merlin.core.dispatch import make_df  # noqa
 from merlin.dag import ColumnSelector  # noqa
-from merlin.io import Dataset  # noqa
-from merlin.schema import ColumnSchema, Schema, Tags  # noqa
-from merlin.systems.dag.ops.fil import PredictForest  # noqa
+from merlin.schema import Tags  # noqa
 from nvtabular import Workflow  # noqa
 from nvtabular import ops as wf_ops  # noqa
 
@@ -167,42 +161,3 @@ def test_workflow_tf_e2e_multi_op_run(tmpdir, dataset, engine):
 
     response = _run_ensemble_on_tritonserver(str(tmpdir), ["output"], df, triton_ens.name)
     assert len(response.as_numpy("output")) == df.shape[0]
-
-
-@pytest.mark.skipif(not TRITON_SERVER_PATH, reason="triton server not found")
-def test_workflow_with_forest_inference(tmpdir):
-    rows = 200
-    num_features = 16
-    X, y = sklearn.datasets.make_regression(
-        n_samples=rows,
-        n_features=num_features,
-        n_informative=num_features // 3,
-        random_state=0,
-    )
-    feature_names = [str(i) for i in range(num_features)]
-    df = pd.DataFrame(X, columns=feature_names, dtype=np.float32)
-    dataset = Dataset(df)
-
-    # Fit GBDT Model
-    model = xgboost.XGBRegressor()
-    model.fit(X, y)
-
-    input_column_schemas = [ColumnSchema(col, dtype=np.float32) for col in feature_names]
-    input_schema = Schema(input_column_schemas)
-    selector = ColumnSelector(feature_names)
-
-    workflow_ops = feature_names >> wf_ops.LogOp()
-    workflow = Workflow(workflow_ops)
-    workflow.fit(dataset)
-
-    triton_chain = selector >> TransformWorkflow(workflow) >> PredictForest(model, input_schema)
-
-    triton_ens = Ensemble(triton_chain, input_schema)
-
-    request_df = df[:5]
-    triton_ens.export(tmpdir)
-
-    response = _run_ensemble_on_tritonserver(
-        str(tmpdir), ["output__0"], request_df, triton_ens.name
-    )
-    assert response.as_numpy("output__0").shape == (5,)
