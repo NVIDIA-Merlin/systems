e6b4521b4609badf5878967765d1f0bd991de034
Oliver Holworthy <oholworthy@nvidia.com>
2022-10-25 16:05:36 +0100

    Add docstrings for `Runtime`

--- merlin/systems/dag/runtimes/base_runtime.py
+++ merlin/systems/dag/runtimes/base_runtime.py
@@ -19,11 +19,40 @@ from merlin.dag.executors import LocalExecutor
 
 
 class Runtime:
+    """A Systems Graph Runtime.
+
+    This class can be used as a base class for custom runtimes.
+    """
+
     def __init__(self, executor=None):
+        """Construct a Runtime.
+
+        Parameters
+        ----------
+        executor : Executor, optional
+            The Graph Executor to use to use for the transform, by default None
+        """
         self.executor = executor or LocalExecutor()
 
     def transform(self, graph: Graph, transformable: Transformable):
+        """Run the graph with the input data.
+
+        Parameters
+        ----------
+        graph : Graph
+            Graph of nodes container operator chains for data manipulation.
+        transformable : Transformable
+            Input data to transform in graph.
+
+        Returns
+        -------
+        Transformable
+            Input data after it has been transformed via graph.
+        """
         return self.executor.transform(transformable, [graph.output_node])
 
     def export(self):
+        """Optional method.
+        Implemented for runtimes that require an exported artifact to transform.
+        """
         raise NotImplementedError
