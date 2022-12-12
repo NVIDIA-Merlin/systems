4bb1f33aa0df349eea63e59737d4d99bf7a062b9
Oliver Holworthy <oholworthy@nvidia.com>
2022-12-12 16:25:35 +0000

    Pass pytorch output_model when exporting pytorch and workflow ensemble

--- merlin/systems/triton/export.py
+++ merlin/systems/triton/export.py
@@ -183,6 +183,7 @@ def export_pytorch_ensemble(
         workflow,
         name + "_nvt",
         preprocessing_path,
+        output_model="pytorch",
         backend=nvtabular_backend,
         cats=cats,
         conts=conts,
