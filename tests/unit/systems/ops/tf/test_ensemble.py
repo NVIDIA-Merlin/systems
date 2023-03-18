caf681f862c236de6a0f2a01c3be5b8010115c84
Karl Higley <kmhigley@gmail.com>
2023-03-18 17:05:13 -0400

    Adjust Tensorflow ensemble tests to use model with correct input shapes
    It turns out you can't build a model where the input shape is only the batch size with the Tensorflow Functional or Sequential APIs—you can only do it by sub-classing `tf.keras.Model`. So this reworks the example models used in the tests to have that shape, and adjust the test expectations to use the resulting output name.

--- tests/unit/systems/ops/tf/test_ensemble.py
+++ tests/unit/systems/ops/tf/test_ensemble.py
@@ -163,9 +163,9 @@ def test_workflow_tf_e2e_multi_op_run(tmpdir, dataset, engine):
     request_schema = workflow.input_schema + workflow_2.input_schema
 
     response = run_ensemble_on_tritonserver(
-        str(tmpdir), request_schema, df, ["output"], ensemble_config.name
+        str(tmpdir), request_schema, df, ["output_1"], ensemble_config.name
     )
-    assert len(response["output"]) == df.shape[0]
+    assert len(response["output_1"]) == df.shape[0]
 
 
 @pytest.mark.skipif(not TRITON_SERVER_PATH, reason="triton server not found")
@@ -220,6 +220,6 @@ def test_workflow_tf_python_wrapper(tmpdir, dataset, engine, python):
     request_schema = workflow.input_schema + workflow_2.input_schema
 
     response = run_ensemble_on_tritonserver(
-        str(tmpdir), request_schema, df, ["output"], ensemble_config.name
+        str(tmpdir), request_schema, df, ["output_1"], ensemble_config.name
     )
-    assert len(response["output"]) == df.shape[0]
+    assert len(response["output_1"]) == df.shape[0]
