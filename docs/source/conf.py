748e05be057ef12e79252a87e10a53f5bd294cf1
Oliver Holworthy <1216955+oliverholworthy@users.noreply.github.com>
2023-05-24 15:18:33 +0100

    Only build docs for tags that match a release pattern

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,6 +96,7 @@ html_show_sourcelink = False
 
 if os.path.exists(gitdir):
     tag_refs = subprocess.check_output(["git", "tag", "-l", "v*"]).decode("utf-8").split()
+    tag_refs = [tag for tag in tag_refs if re.match(r"^v[0-9]+.[0-9]+.[0-9]+$", tag)]
     tag_refs = natsorted(tag_refs)[-6:]
     smv_tag_whitelist = r"^(" + r"|".join(tag_refs) + r")$"
 else:
